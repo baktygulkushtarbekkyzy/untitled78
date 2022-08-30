0959dc007c50a4125183d9883e66dd414f7a5236
user <baktygulkushtarbekkyzy@gmail.com>
2022-08-30 14:44:00 +0600

    ini

---
+++ src/Circle.java
@@ -0,0 +1,14 @@
+public class Circle {
+    private int Pi;
+
+
+    static void area(int radius){
+        System.out.print("Tegerektin ayanty: ");
+        System.out.println(Math.PI*(radius*radius));
+    }
+    static void circumference(int radius){
+        System.out.print("Tegerektin uzundugu: ");
+        System.out.println(Math.PI*2*radius);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Circle.area(3);
+        Circle.circumference(3);
+
+
+
+    }
+}
